f44ef24d8a76c0ac19dbd3ded6b0d4e8fd8f3300
EvaldoFires <125889031+EvaldoFires@users.noreply.github.com>
2025-07-08 15:36:29 -0300

    Inicio do projeto criado com Spring Initializer

---
+++ src/main/java/br/com/evaldo/estudo/gerenciador_usuario/EstudoGerenciadorDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.evaldo.estudo.gerenciador_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoGerenciadorDeUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoGerenciadorDeUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/evaldo/estudo/gerenciador_usuario/EstudoGerenciadorDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.evaldo.estudo.gerenciador_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoGerenciadorDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
